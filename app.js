2ce31aa6aec1e2ac0cfde2e522ec902ce02f712c
Robert Mangum <rmangum@GMC.local>
2019-11-06 16:46:26 -0700

    adding errors

--- app.js
+++ app.js
@@ -5,14 +5,53 @@ const dotenv = require('dotenv'),
     db = require('pg');
 //import environmental    
 dotenv.config();
+const PORT = process.env.PORT || 5000
+const connect = process.env.CONNECT
+const pool = new db.Pool({
+    connectionString: connect
+})
+
 //ejs
 app.use(exLayouts);
 app.set('view engine', 'ejs')
+//bodyparsers
+app.use(ex.urlencoded({
+    extended: false
+}));
 
-const PORT = process.env.PORT || 5000
+
+/*app.get('/', (req, res) => {
+    console.log('someone is connecting!')
+    pool.connect((err, client, done) => {
+        if (err) {
+            res.sendStatus(500);
+            return console.error(`error fetching client from pool: ${err}`);
+        }
+        client.query('SELECT * FROM accounts', (err, result) => {
+
+            if (err) {
+                return console.error(`error running query: ${err}`);
+            }
+            res.send(
+                result
+            )
+            done();
+        })
+    })
+})
+*/
+//connect to db
+pool.connect((err, client, done) => {
+    if (err) {
+        res.sendStatus(500);
+        return console.error(`error fetching client from pool: ${err}`);
+    }
+    console.log('DB Connected')
+})
+
 //routes 
 app.use('/', require('./routes/index'))
 app.use('/users', require('./routes/users'))
-app.listen(PORT, console.log(`Listenign on port: ${PORT}`))
+app.listen(PORT, console.log(`Listening on port: ${PORT}`))
 
-//https://youtu.be/6FOq4cUdH8k?t=1359
+//https://youtu.be/6FOq4cUdH8k?t=2188
--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,45 @@ const express = require('express'),
 
 router.get('/login', (req, res) => {
     res.render('login');
-})
+});
 router.get('/register', (req, res) => {
     res.render('register');
-})
+});
+router.post('/register', (req, res) => {
+    const {
+        name,
+        email,
+        password,
+        password2
+    } = req.body
+    let errors = [];
+    //check required feilds
+    if (!name || !email || !password || !password2) {
+        errors.push({
+            msg: 'Please fill in all feilds'
+        });
+    }
+    if (password != password2) {
+        errors.push({
+            msg: 'Password does not match.'
+        });
+    }
+    if (password.length < 6) {
+        errors.push({
+            msg: 'Password needs to be at least 6 characters.'
+        });
+    }
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        })
+    } else {
+        res.send('pass')
+    }
+});
+
 module.exports = router
